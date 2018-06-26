feb32329928b81ef0abe19d9900fa6e0afea5bc0
Kanyango <kariukikanyango@gmail.com>
2018-06-26 19:49:02 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  define('ROOT_URL','');
+//This is the config.php file
+// Specify your login credentials
+$username   = "KingBean";
+$apikey     = "162aadf83fa81a3632d00c0b4ee19ee18f2f04f5f43979b6d27401627a1e2df8";
+?>
