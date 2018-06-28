e14beab516773c92cfd7a1e63993b5165b5c3b9f
Kanyango <kariukikanyango@gmail.com>
2018-06-28 18:05:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ else if ( $text == "1" ) {
 header('Content-type: text/plain');
 
 
-$url = 'http://fec43943.ngrok.io/lengoStreamx/ussds';
+$url = 'http://fec43943.ngrok.io/lengostreamx/ussds';
 
 $data = array( 
               'serviceCode' => $serviceCode, 
